c9b7cce80eb2034a955a21815530ee5129baeb06
kevin-mehta <kevin.h.mehta@gmail.com>
2021-06-16 13:01:09 +0530

    2 files modified

--- src/app/pipes/search/search.pipe.ts
+++ src/app/pipes/search/search.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'LockFilter'
+  name: 'SearchPipe'
 })
 export class SearchPipe implements PipeTransform {
   transform(value: any, args?: any): any {
